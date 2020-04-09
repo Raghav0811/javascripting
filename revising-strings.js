39006ae62d108e09d01e3872beb3eaac9fd6e95d
vagrant <raghavbpdshrivastav@gmail.com>
2020-04-09 16:21:39 +0000

    Initial commit with all my work so far

---
+++ revising-strings.js
@@ -0,0 +1,10 @@
+//const pizza = `pizza is alright`;
+//pizza = pizza.replace(`alright`, `wonderful`);
+//console.log(pizza);
+//const pizza = `pizza is alright`;
+//pizza=pizza.replace(`alright`,`wonderful`);
+let pizza = `pizza is alright`;
+pizza = pizza.replace(`alright`, `wonderful`);
+console.log(pizza);
+
+
---
+++ strings.js
@@ -0,0 +1,4 @@
+//const string = `This is a string`;
+//console.log(string);
+const someString = `this is a string`;
+console.log(someString);
